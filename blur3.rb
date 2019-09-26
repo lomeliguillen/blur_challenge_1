40ac92078824275e759c8c9833b216d0a9961b6f
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-09-26 15:16:39 +0000

    Blur 3 challenge finished

--- blur3.rb
+++ blur3.rb
@@ -18,10 +18,8 @@ class Image
 
   # blur 2 challenge
   def blur
-  
-    
 
-    image.each_with_index do |row, row_index|
+    @image.each_with_index do |row, row_index|
       row.each_with_index do |value, column_index|
 
         if value == 1 
@@ -42,40 +40,15 @@ class Image
     end   
   end
 # blur 3 challenge
-=begin
-  def distance
-  
-    blured = Marshal.load(Marshal.dump(image))
-
-    image.each_with_index do |row, row_index|
-      row.each_with_index do |value, column_index|
-
-        if value == 1
-
-          blured[row_index][column_index+1] = 1 if row.size > column_index + 1
-          blured[row_index - 1][column_index] = 1 if row_index > 0
-          blured[row_index + 1][column_index] = 1 if image.size > row_index + 1
-          blured[row_index][column_index-1] = 1 if column_index > 0
-
-        end
-      end
-    end
-          
-    blured.each do |row| 
-      row.each do |coumn| 
-        print coumn.to_s
-      end 
-      puts 
-    end     
-  end
-
 
   def distance_blur(distance)
     distance.times do
       blur
+      @image = Marshal.load(Marshal.dump(@blured))
     end
   end
-=end
+
+
 end
 
 
@@ -92,11 +65,11 @@ image = Image.new([
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ])
 
+image.distance_blur(4)
 image.output_blur
-
 
